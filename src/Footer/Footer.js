556349c5b019ca5ac8df33479039e32f8e941902
WesTheHuman2000 <jlin22@uncc.edu>
2023-09-26 15:24:20 -0400

    adding pb react part 1

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer aria-orientation="horizontal" className="bottom" >
+        <div className="center">
+            All rights reserved &copy; Wesley Lin
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
